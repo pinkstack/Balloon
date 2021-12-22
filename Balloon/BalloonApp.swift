61541c5faa9bc6fa5e88db73929e2d0f394c674b
Oto Brglez <otobrglez@gmail.com>
2021-12-22 23:54:37 +0100

    Initial Commit

---
+++ Balloon/BalloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonApp.swift
+//  Balloon
+//
+//  Created by Oto Brglez on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BalloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
