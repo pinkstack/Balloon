86c5bad04d2af00ff004bd79040b8e8f99bdd9cd
Oto Brglez <otobrglez@gmail.com>
2021-12-23 13:46:06 +0100

    Polishing output.

--- Balloon/ContentView.swift
+++ Balloon/ContentView.swift
@@ -10,23 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var tocenLive: TocenLive
 
+    func printKind(kind: Event.Kind) -> String {
+        switch kind {
+            case .Added: return "Added"
+            case .Removed: return "Removed"
+            case .Updated: return "Updated"
+        }
+    }
     
     var body: some View {
         ScrollView {
             ForEach(tocenLive.events.reversed(), id:\.id) { event in
                 VStack {
                     HStack {
-                        Image(systemName: "bus")
-                        Text("\(event.data.busInfo.bus_name)").font(.title)
+                        Image(systemName: "info.circle.fill")
+                        Text("\(event.data.busInfo.bus_name) @ \(event.data.busInfo.bus_group)")
+                            .bold()
                     }
                     HStack {
+                        Text("\(printKind(kind: event.kind))")
                         Text(event.created_at, style: .time)
+                        Text(String(format:"%.2f km/h", event.data.busInfo.velocity))
+                        
                     }
                     Rectangle()
                         .frame(height: 1.0, alignment: .bottom)
                         .foregroundColor(Color.purple)
                 }
-                .padding()
+                // .padding()
             }
             
             if (tocenLive.events.count == 0) {
