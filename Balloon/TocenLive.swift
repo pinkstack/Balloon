d45a5aec55d61b3048bbb3785c82f7dc07fe382f
Oto Brglez <otobrglez@gmail.com>
2021-12-23 13:23:00 +0100

    Refactoring.

--- Balloon/TocenLive.swift
+++ Balloon/TocenLive.swift
@@ -4,44 +4,8 @@
 //
 //  Created by Oto Brglez on 23/12/2021.
 //
-import SwiftUI
 import Foundation
 import Starscream
-
-struct Event: Codable {
-    enum Kind: String, Codable {
-        case Added, Updated, Removed
-    }
-    
-    struct BusInfo: Codable {
-        let bus_id: UUID
-        let bus_name: String
-        let bus_group: String
-        let lat: Double
-        let lng: Double
-        let rotation: Double
-        let velocity: Double
-    }
-    
-    struct EventData: Codable {
-        let busInfo: BusInfo
-        let previous: BusInfo?
-    }
-    
-    let kind: Kind
-    let id: UUID
-    let created_at: Date
-    let data: EventData
-    
-    static func fromText(text: String) -> Event {
-        let decoder = JSONDecoder()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
-        decoder.dateDecodingStrategy = .formatted(dateFormatter)
-        let event = try! decoder.decode(Event.self, from: text.data(using: .utf8)!)
-        return event
-    }
-}
 
 class TocenLive: ObservableObject, WebSocketDelegate {
     @Published private(set) var isConnected = false
