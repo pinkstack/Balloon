4c3f2bf3d1f695a7d6827b14752a387c1c31689a
Oto Brglez <otobrglez@gmail.com>
2021-12-24 00:55:53 +0100

    Small fixes.

--- Balloon/BusesView.swift
+++ Balloon/BusesView.swift
@@ -54,6 +54,43 @@ struct BusesView: View {
     }
 }
 
+extension BusesView {
+    class ViewModel: ObservableObject {
+        @Published var buses = [BusInfo]()
+        private var tocenLive = TocenLive()
+        var eventCancellable: AnyCancellable?
+
+        func connect() {
+            tocenLive.connect()
+            eventCancellable = tocenLive.currentEvent
+                .filter { $0.kind == Event.Kind.Updated }
+                .map { $0.data.busInfo }
+                .sink { [weak self] busInfo in
+                    self?.updateBusInfo(busInfo: busInfo)
+                }
+        }
+        
+        func updateBusInfo(busInfo: BusInfo) {
+            if let busInfoIndex = self.buses.firstIndex(where: {
+                $0.bus_id == busInfo.bus_id
+            }) {
+                self.buses[busInfoIndex] = busInfo
+            } else {
+                self.buses.append(busInfo)
+                self.buses.sort(by: {
+                    $0.bus_group > $1.bus_group
+                    // && $0.bus_name > $1.bus_name
+                })
+            }
+        }
+        
+        func disconnect() {
+            tocenLive.disconnect()
+            eventCancellable?.cancel()
+        }
+    }
+}
+
 struct BusesView_Previews: PreviewProvider {
     static var previews: some View {
         let vm = BusesView.ViewModel()
